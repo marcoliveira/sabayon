62b091b1d310a82900fa906084834b0c740b9d07
Mark McLoughlin <mark@skynet.ie>
2005-01-26 18:31:03 +0000

    pull username and homedir from the passwd database if available.
    2005-01-26  Mark McLoughlin  <mark@skynet.ie>
    
            * lib/util.py: pull username and homedir from
            the passwd database if available.

--- lib/util.py
+++ lib/util.py
@@ -19,30 +19,37 @@
 #
 
 import os
+import pwd
 
 class GeneralError (Exception):
     def __init__ (self, msg):
         Exception.__init__ (self, msg)
 
 def get_home_dir ():
-    #
-    # FIXME: try pull $HOME from the user's passwd entry rather
-    #        than from os.environ - just like g_get_home_dir()
-    #
+    try:
+        pw = pwd.getpwuid (os.getuid ())
+        if pw.pw_dir != "":
+            return pw.pw_dir
+    except KeyError:
+        pass
+    
     if os.environ.has_key ("HOME"):
         return os.environ["HOME"]
     else:
-        raise GeneralError ("No value for $HOME found in environment - cannot find home directory")
+        raise KeyError ("Cannot find home directory: not set in /etc/passwd and no value for $HOME in environment")
 
 def get_user_name ():
-    #
-    # FIXME: pull username from the user's passwd entry rather
-    #        than from os.environ - just like g_get_user_name()
-    #
+    try:
+        pw = pwd.getpwuid (os.getuid ())
+        if pw.pw_name != "":
+            return pw.pw_name
+    except KeyError:
+        pass
+    
     if os.environ.has_key ("USER"):
         return os.environ["USER"]
     else:
-        raise GeneralError ("No value for $USER found in environment - cannot username")
+        raise KeyError ("Cannot find username: not set in /etc/passwd and no value for $USER in environment")
 
 def print_exception ():
     import traceback
@@ -50,21 +57,5 @@ def print_exception ():
     traceback.print_exc(file=sys.stderr)
 
 def run_unit_tests ():
-    def test_env_func (func, key):
-        old_value = os.environ[key]
-        os.environ[key] = "foo"
-
-        assert func () == "foo"
-        del os.environ[key]
-
-        got_exception = False
-        try:
-            func ()
-        except GeneralError:
-            got_exception = True
-        assert got_exception
-
-        os.environ[key] = old_value
-
-    test_env_func (get_home_dir, "HOME")
-    test_env_func (get_user_name, "USER")
+    assert get_home_dir () != ""
+    assert get_user_name () != ""
