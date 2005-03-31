db3709375a8e0918429c583b2505f5ccc14ca260
John Dennis <jdennis@src.gnome.org>
2005-03-31 23:53:55 +0000

    add functionality to mozilla_bookmarks, see Changelog

--- lib/mozilla_bookmarks.py
+++ lib/mozilla_bookmarks.py
@@ -19,8 +19,17 @@
 #
 
 import sys
-
+import time
+import os
+import re
 from HTMLParser import HTMLParser
+
+try:
+    import util
+    import config
+except:
+    from sabayon import util
+    from sabayon import config
 
 debug = 0
 indent = '    '
@@ -42,6 +51,36 @@ tag_info_dict = {
     'hr' : {'simple_tag'           : True},
 }
 
+# XXX - these should be defined one place
+LOG_OPERATION           = 0x00001
+LOG_CHANGE              = 0x00002
+LOG_IGNORED_CHANGE      = 0x00004
+LOG_APPLY               = 0x00008
+LOG_SYNC                = 0x00010
+LOG_PARSE               = 0x00020
+LOG_PREF                = 0x00040
+LOG_FILE_CONTENTS       = 0x00080
+LOG_DATA                = 0x00100
+LOG_VERBOSE             = 0x10000
+
+def dprint(mask, fmt, *args):
+    util.debug_print(util.DEBUG_MOZILLASOURCE, fmt % args, mask)
+
+exclude_attrs = [re.compile("^id$"),
+                 re.compile("^last_"),
+                 re.compile("^add_"),
+                 re.compile("^icon$"),
+                 ]
+
+def filter_attr(attr, exclude):
+    if not exclude:
+        return False
+
+    for regexp in exclude:
+        if regexp.search(attr):
+            return True
+    return False
+
 class Bookmark:
     def __init__(self, folder, name):
         self.folder = folder
@@ -50,6 +89,9 @@ class Bookmark:
 
     def get_attr(self, name):
         return self.attrs.get(name, None)
+
+    def url(self):
+        return self.attrs.get("href", None)
 
     def path(self):
         path = self.folder.path()
@@ -129,6 +171,66 @@ class BookmarkFolder:
         self.traverse(visit)
         return result
     
+    def convert_to_dict(self):
+        result = {}
+
+        def visit(folder, path, data):
+            for bookmark in folder.bookmarks:
+                bookmark_path = bookmark.path_as_names(" -> ")
+                bookmark_url = bookmark.url()
+                result[bookmark_path] = bookmark_url
+
+        self.traverse(visit)
+        return result
+    
+    # XXX - need to separate parsing from file object
+    def write(self, full_path=None, exclude_attrs=None):
+        #if not full_path:
+        #    full_path = self.get_full_path()
+        #dir = os.path.dirname(full_path)
+        #if not os.path.exists(dir):
+        #    os.makedirs(dir)
+        #dprint(LOG_OPERATION, "MozillaBookmark: writing file (%s)", full_path)
+        # XXX fd = open(full_path, "w")
+        fd = sys.stdout
+        fd.write('''
+<!DOCTYPE NETSCAPE-Bookmark-file-1>
+<!-- This is an automatically generated file. (Created by %s, version %s)
+     It will be read and overwritten.
+     DO NOT EDIT! -->
+<META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8">
+<TITLE>Bookmarks</TITLE>
+<H1 LAST_MODIFIED="%.0f">Bookmarks</H1>
+
+<DL><p>
+''' % (config.PACKAGE, config.VERSION, time.time()))
+
+        def visit(folder, path, data):
+            indent = "    "
+            folder_path = folder.path()
+            level = len(folder.path())
+            fd.write("%s<DT><H3" % (indent*level))
+            for attr, value in folder.attrs.items():
+                if not filter_attr(attr, exclude_attrs):
+                    fd.write(" %s=\"%s\"" % (attr, value))
+            fd.write(">%s</H3>\n" % (folder.name))
+            fd.write("%s<DL><p>\n" % (indent*level))
+
+            level += 1
+            for bookmark in folder.bookmarks:
+                fd.write("%s<DT><A" % (indent*level))
+                for attr, value in bookmark.attrs.items():
+                    if not filter_attr(attr, exclude_attrs):
+                        fd.write(" %s=\"%s\"" % (attr, value))
+                fd.write(">%s</A>\n" % (bookmark.name))
+
+            level -= 1
+            fd.write("%s</DL><p>\n" % (indent*level))
+
+        self.traverse(visit)
+        fd.close()
+        
+
 # ----------------------------------
 
 class HTMLTag:
@@ -273,18 +375,36 @@ def visit(folder, path, data=None):
 # -----------------------
 
 if __name__ == "__main__":
-    parser = BookmarkHTMLParser()
+    bm_file = BookmarkHTMLParser()
+    bm_file.feed(open('bookmarks.html').read())
+    bm_file.close()
 
-    parser.feed(open('bookmarks.html').read())
-    parser.close()
+    bm_file_1 = BookmarkHTMLParser()
+    bm_file_1.feed(open('bookmarks1.html').read())
+    bm_file_1.close()
 
-    bm_name = "libical"
-    bm_list = parser.folder_root.find_bookmark(bm_name)
-    if bm_list:
-        for bm in bm_list:
-            print "found bookmark %s url=%s" % (bm.name, bm.get_attr("href"))
-            print "path = %s" % bm.path_as_names(" -> ")
-        else:
-            print "%s not found" % bm_name
+    if False:
+        bm_name = "libical"
+        bm_list = bm_file.folder_root.find_bookmark(bm_name)
+        if bm_list:
+            for bm in bm_list:
+                print "found bookmark %s url=%s" % (bm.name, bm.get_attr("href"))
+                print "path = %s" % bm.path_as_names(" -> ")
+            else:
+                print "%s not found" % bm_name
 
-    parser.folder_root.traverse(visit)
+    if False:
+        bm_file.folder_root.traverse(visit)
+
+    if False:
+        bm_dict   = bm_file.folder_root.convert_to_dict()
+        bm_dict_1 = bm_file_1.folder_root.convert_to_dict()
+
+        dc = DictCompare(bm_dict, bm_dict_1)
+        dc.compare()
+        cs = dc.get_change_set('a', 'b')
+        dump_change_set(cs)
+
+    if True:
+        bm_file.folder_root.write("tmp_bookmarks.html", exclude_attrs=exclude_attrs)
+
